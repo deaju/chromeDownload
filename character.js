5056a4ddbc5537983f4711fee9c5e40a683c9962
unknown <ganbaru.wakenai@gmail.com>
2017-11-27 21:47:08 +0900

    calc progress

--- character.js
+++ character.js
@@ -1,23 +1,65 @@
+const imageCancel = 'image/cancel/';
+const imageStart = 'image/start/';
+const imageTest = 'image/test/';
+const MAX_NUM=50;
+const IMAGE_SIZE=160;
+
 $(function () {
     'use strict';
 
     chrome.extension.onRequest.addListener(
         (request, sender, sendResponse) => {
-            $('body')
-            .append(
-                '<div class="eye-animation__wrapper"><div class="eye-animation"></div></div>');
-            alert("step2 " + request.greeting);
-            $('.eye-animation').on('click',()=>{
-                $('.eye-animation__wrapper').remove();
-            });
-              
             //★ここに★ 表示しているサイトのベースカラーを変更する処理を記述する。
               
-            if (request.greeting == "hello")
-                sendResponse({farewell: "goodbye"});
-            else
-                //★ここ重要★ レスポンスがない場合でも、必ず空のオブジェクトを返す。
-                sendResponse({}); // snub them.
-            });
+            if (request.download === "start"){
+                $('body')
+                .append(
+                    '<div class="character_wrapper"><div class="character-animation"></div></div>');
+                $('.character-animation').on('click',()=>{
+                   // $('.character_wrapper').remove();
+                });
+                changeImage(imageStart);
+                sendResponse({character: "show"});
+            }
+            if(request.downloadSpeed >= 0){
+                console.log('speed '+request.downloadSpeed / (1024*1024) );
+            }
+            if(request.downloadProgress >= 0 ){
+                console.log('progress '+request.downloadProgress);
+            }
+        }
+    );
+
+    function changeImage(srcImage){
+        let imageUrl = '';
+        let imageSize = '';
+        for(let i=1; i < MAX_NUM;i++){
+            imageUrl = imageUrl + 'url(' + chrome.extension.getURL(srcImage + ( '0000' + i ).slice( -4 ) + '.png')+'),';
+            imageSize = imageSize + IMAGE_SIZE * (i -1) + 'px top,'
+        }
+        imageUrl = imageUrl + 'url(' + chrome.extension.getURL( srcImage + ( '0000' + MAX_NUM ).slice( -4 ) + '.png') +')';
+        imageSize = imageSize + IMAGE_SIZE * (MAX_NUM -1) + 'px top,';
+        imageSize = imageSize + IMAGE_SIZE * MAX_NUM + 'px top';
+        console.log(imageSize);
+        $('.character-animation').css('width',IMAGE_SIZE*MAX_NUM + 'px');
+        $('.character-animation').css('animation-timing-function','steps(' + MAX_NUM + ')');
+        $('.character-animation').css('background-image',imageUrl);
+        $('.character-animation').css('background-position',imageSize);
+    }
+    /*
+    function changeImage(srcImage){
+        let imageUrl = '';
+        let imageSize = '';
+        for(let i=1; i < MAX_NUM;i++){
+            imageUrl = imageUrl + 'url(' + chrome.extension.getURL(srcImage + i + '.png')+'),';
+            imageSize = imageSize + IMAGE_SIZE * (i -1) + ' top,'
+        }
+        imageUrl = imageUrl + 'url(' + chrome.extension.getURL( srcImage + MAX_NUM + '.png') +')';
+        imageSize = imageSize + IMAGE_SIZE * (MAX_NUM -1) + ' top,'
+        imageSize = imageSize + IMAGE_SIZE * MAX_NUM + ' top'
+        $('.character-animation').css('background-image',imageUrl);
+        $('.character-animation').css('background-position','0 top,160px top,320px top,480px top,640px top,800px top,960px top,1120px top,1280px top,1440px top');
+    }
+    */
 
 });
--- download.js
+++ download.js
@@ -1,11 +1,79 @@
+let character = [];
+let timer;
+let tabInfo;
 
-chrome.downloads.onCreated.addListener(function (){
-    chrome.tabs.getSelected(null, function(tab) {
-        // ★現在選択中のtab.idが必要なので、getSelectedメソッドの中にsendRequestを記述する。
-        alert("step1");
-        chrome.tabs.sendRequest(tab.id, {greeting: "hello"}, function(response) {
+class Character {
+    constructer (){
+        this._speed = 0;
+        this._progress=0;
+    }
+    set downloadItem(downloadItem){
+        this._downloadItem = downloadItem;
+    }
+    get downloadItem() {
+        return this._downloadItem;
+    }
+    get speed(){
+        return this._speed;
+    }
+    set speed(speed){
+        this._speed = speed;
+    }
+    get progress(){
+        return this._progress;
+    }
+    set progress(progress){
+        this._progress = progress;
+    }
+
+    calcDownloadSpeed(downloadItem){
+        this._speed = downloadItem.bytesReceived - this._downloadItem.bytesReceived;
+    }
+    calcDownloadProgress(downloadItem){
+        this._progress= (downloadItem.bytesReceived / downloadItem.totalBytes)*100;
+    }
+}
+
+chrome.downloads.onCreated.addListener( startDownload );
+
+
+
+
+function startDownload(downloadItem) {
+    chrome.tabs.getSelected(null, (tab) => {
+        tabInfo = tab;
+            // ★現在選択中のtab.idが必要なので、getSelectedメソッドの中にsendRequestを記述する。
+        chrome.tabs.sendRequest(tab.id, {download: "start"}, (response) => {
             // ここに受信側からレスポンスが返ってきた時にする処理を記述する。
-            alert("step3 " + response.farewell);
         });
-    });    
-});
+    }); 
+    let tmpC = new Character();
+    tmpC.downloadItem=downloadItem;
+    character.push(tmpC);
+    timer = setInterval(getDownloadObject,1000);
+}
+
+function getDownloadObject(){
+    if( character.length > 0 ){
+        let index = 0;
+        let tmpChara = character[index];
+        let query = {id:tmpChara.downloadItem.id};
+        chrome.downloads.search(query,(downloadItems)=>{
+            let downloadItem = downloadItems[index];
+            if(downloadItem.state === 'complete' || downloadItem.state === 'interrupted'){
+                clearInterval(timer);
+                tmpChara.downloadItem = downloadItem;
+            } else {
+                tmpChara.calcDownloadSpeed(downloadItem);
+                tmpChara.calcDownloadProgress(downloadItem);
+                tmpChara.downloadItem = downloadItem;
+                let request = {
+                    downloadSpeed: tmpChara.speed,
+                    downloadProgress:tmpChara.progress
+                };
+                chrome.tabs.sendRequest(tabInfo.id, request);
+            }
+         });
+    }
+}
+
